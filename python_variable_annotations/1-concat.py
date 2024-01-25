6832ae4409cafc1a0223a15cbc7d6d7f5275913c
benjamin ontiveros <bontiveros15@gmail.com>
2024-01-25 09:53:01 -0800

    feaT: annotation string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Handle of string with annotations
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Args:
+            str1: the first string parameter
+            str1: the second string parameter
+
+        return:
+            concatened string
+    """
+    return (str1 + str2)
