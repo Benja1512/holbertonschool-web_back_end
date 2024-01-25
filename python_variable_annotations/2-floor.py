e2c895af0fe7aac5e8605eab5ec7e620c644a64e
benjamin ontiveros <bontiveros15@gmail.com>
2024-01-25 10:28:59 -0800

    feat: float truncated integer annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    A function that recieve a float n
+"""
+
+def floor(n: float) -> int:
+    """
+        Args: 
+            n: float number
+
+        Return:
+            Float number truncated to floor
+    """
+
+    return int(n)
