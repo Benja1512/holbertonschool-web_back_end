e54806d6a5c440dc3d5c2975a627ccea78b386d3
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-11 20:45:04 -0500

    fixed: code slint

--- ES6_classes/0-classroom.test.js
+++ ES6_classes/0-classroom.test.js
@@ -1,10 +1,6 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from "./0-classroom.js";
 
-beforeEach(() => {
-  // Cualquier configuración necesaria antes de cada prueba
-});
-
-test('Initialize an instance of ClassRoom set to 100', () => {
+test("Initialize an instance of ClassRoom set to 100", () => {
   const room = new ClassRoom(100);
   expect(room._maxStudentsSize).toBe(100);
 });
