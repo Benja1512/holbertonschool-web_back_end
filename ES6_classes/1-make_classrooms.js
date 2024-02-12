1e60b2219f0ee1544e735289ed2a68a501348471
Benjamin Ontiveros <bontiveros15@gmail.com>
2024-02-11 19:03:37 -0500

    feat: code fixed

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,10 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+export default function initializeRooms() {
+  const classRooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return classRooms;
 }
-
-export default initializeRooms;
