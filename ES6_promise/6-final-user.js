d3ff4bbbc7a89709fd917c1a6859bf7b46f2b9df
benjamin ontiveros <bontiveros15@gmail.com>
2024-02-23 12:47:34 -0800

    fixed: coded

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,20 @@
-// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((promises) => {
+    const result = [];
+    for (const { status, value, reason } of promises) {
+      result.push({
+        status,
+        value: value || reason.toString(),
+      });
+    }
+    return result;
+  });
 }
 
 export default handleProfileSignup;
