1293b806fcb1917c074ea677e72d5d2fa609e05e
Benja1512 <bontiveros15@gmail.com>
2024-05-02 10:34:48 -0500

    feat: pagination: start_index and end_index with tuple

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Args:
+        page (int): [description]
+        page_size (int): [description]
+
+    Returns:
+        Tuple[int, int]: [description]
+    """
+    start_index = (page * page_size) - page_size
+    end_index = page * page_size
+    return ((start_index, end_index))
