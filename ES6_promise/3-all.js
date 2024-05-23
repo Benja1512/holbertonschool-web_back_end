5ec5a6d07a8e3168fa3537208a471b757945b703
Benja1512 <bontiveros15@gmail.com>
2024-05-23 06:16:06 -0500

    fixed: passed eslint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
     .then((values) => {
       const data = { ...values[0], ...values[1] };
