85f80d962826e2c06e90ad980658953b0edeb31e
Benja1512 <benjamin.ontiveros@upsjb.edu.pe>
2024-05-28 19:53:59 -0500

    fix: true not

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('the fake API is not working currently'));
